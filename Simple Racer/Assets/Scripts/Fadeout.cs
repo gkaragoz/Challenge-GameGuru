7a1ccb9b0130e29103c17894d20fd6bacdeb4294
gkaragoz <gkaragoz96@gmail.com>
2020-10-03 13:11:18 +0300

    Add Fadeout script.

---
+++ Simple Racer/Assets/Scripts/Fadeout.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace SimpleRacer {
+
+	[RequireComponent(typeof(CanvasGroup))]
+	public class Fadeout : MonoBehaviour {
+
+		[Header("Initializations")]
+		[SerializeField]
+		private float _delayTime = 0.5f;
+		[SerializeField]
+		private float _time = 1f;
+		[SerializeField]
+		private CanvasGroup _canvasGroup = null;
+
+		private void Start() {
+			_canvasGroup.alpha = 1f;
+
+			LeanTween.cancel(this.gameObject);
+
+			this.gameObject.LeanDelayedCall(_delayTime, () => {
+				this.gameObject.LeanValue(1f, 0f, _time)
+					.setOnUpdate((float value) => {
+						_canvasGroup.alpha = value;
+					});
+			});
+		}
+
+	}
+
+}
